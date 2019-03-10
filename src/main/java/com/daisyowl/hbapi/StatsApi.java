1665f774b15e88a1251d4688168bb2c1acffa982
Ben Wiklund <ben@daisyowl.com>
2019-03-09 21:08:50 -0800

    get

--- src/main/java/com/daisyowl/hbapi/StatsApi.java
+++ src/main/java/com/daisyowl/hbapi/StatsApi.java
@@ -1,14 +1,23 @@
 package com.daisyowl.hbapi;
 
+import com.daisyowl.hbapi.models.Stats;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api/v1/stats")
 public class StatsApi {
-    @RequestMapping(value = "hi", method = RequestMethod.GET)
-    public String hi(){
-        return "good day sir";
+    @RequestMapping(value = "hi", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Stats hi(){
+        Stats stats = new Stats();
+        stats.userId = 1234;
+        stats.myStrings = new ArrayList<>();
+        stats.myStrings.add("hello");
+        stats.myStrings.add("hi");
+        return stats;
     }
 }
---
+++ src/main/java/com/daisyowl/hbapi/models/Stats.java
@@ -0,0 +1,9 @@
+package com.daisyowl.hbapi.models;
+
+import java.util.List;
+
+public class Stats {
+    public float score;
+    public int userId;
+    public List<String> myStrings;
+}
